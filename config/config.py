af4bd3097d824154609f8d77d74540f0f4ead629
mattialosz <mattia.loszach@gmail.com>
2025-08-05 17:01:30 +0200

    Ollama Model Setup

---
+++ config/config.py
@@ -0,0 +1,53 @@
+import os
+import json
+from .ollama_model import choose_model
+from utils.graphic import BLUE, YELLOW, RESET
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+CONFIG_DIR = os.path.join(BASE_DIR, "preferences")
+CONFIG_FILE = os.path.join(CONFIG_DIR, "user_config.json")
+
+def create_config():
+    """Function for creation of config file"""
+    if not os.path.exists(CONFIG_DIR):
+        os.makedirs(CONFIG_DIR) # Create config directory, if non existing yet
+    
+    print(
+        f"\n  {BLUE}\033[1mSetup with Ollama\033[0m{RESET}\n\n"
+        "When using \033[1mRISCV-GPT\033[0m, your agent relies on a base model that must be selected. "
+        "Your model runs locally via the Ollama service. Make sure Ollama is installed correctly "
+        "(for more information visit 'https://ollama.com')\n\n"
+        "Once you've selected a base model, it will be saved in a configuration file located at "
+        "\033[1m'RISCV-GPT/preferences/config.json'\033[0m\n\n"
+        "If you want to change the model later, just update the value of the 'model_name' key in that file.\n"
+        "It is generally recommended to use models like 'llama3:latest'."
+    )
+    model_name = choose_model()
+    config_data = {"model_name": model_name}
+
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(config_data, f, indent=4)
+    
+    print(f"    \n↪ Config file created at {CONFIG_FILE}")
+    
+
+def load_config():
+    """Loads existing configurations"""
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "r") as f:
+            config_data = json.load(f)
+        return config_data.get("model_name")
+    else:
+        return None
+
+def setup():
+    model_name = load_config()
+
+    if model_name == None: # Config file not found
+        create_config()
+        model_name = load_config() # Config data can now be accessed
+    info_model = f"    {YELLOW}\033[1m↪ Using model: \033[1m{model_name}\033[0m{RESET}"
+    return info_model, model_name
+
+if __name__ == "__main__":
+    setup()
--- rag/core.py
+++ rag/core.py
@@ -3,9 +3,11 @@ import threading
 from .prompt_builder import prompt
 from utils.graphic import BLUE, YELLOW, RESET
 from utils.graphic import print_logo, draw_box, spinner
+from config.config import setup
 #from vector import retriever
 
-model = OllamaLLM(model="llama3") # Base Model from Ollama
+info_model, model_name = setup()
+model = OllamaLLM(model=model_name) # Base Model from Ollama
 
 chain = prompt | model # Pipeline using Langchain
 
@@ -13,15 +15,16 @@ exit_kw = ["/q", "/quit", "/exit"]
 
 def chat():
     print_logo()
-    print("    \033[38;5;250m↳ Type your question here (or type '/q', '/quit', '/exit' to quit):\n\033[0m")
+    print(info_model)
+    print("    \033[38;5;250m↪ Type your question here (or type '/q', '/quit', '/exit' to quit):\n\033[0m")
 
     while True:
         question = input(f"{YELLOW}❯ {RESET}").strip()
         if question.lower() in exit_kw:
-            print("\n    \033[38;5;250m↳ Goodbye! See you soon.\033[0m\n")
+            print("\n    \033[38;5;250m↪ Goodbye! See you soon.\033[0m\n")
             break
         if question.startswith("/"):
-            print("\n    \033[38;5;250m↳ Type your question here (or type '/q', '/quit', '/exit' to quit):\n\033[0m")
+            print("\n    \033[38;5;250m↪ Type your question here (or type '/q', '/quit', '/exit' to quit):\n\033[0m")
             continue
         if not question:
             continue
